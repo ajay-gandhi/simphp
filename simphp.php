ef680ee27d207593cd65fc85c3c53f6c965666bd
Ajay Gandhi <aag255@cornell.edu>
2014-10-22 15:11:35 -0400

    Refactor code to follow more best practices

--- simphp.php
+++ simphp.php
@@ -1,17 +1,17 @@
-<?
+<?php
 
 /*----------------------------
 
 -------- ++ simPHP ++ --------
-A simple PHP hit counter.
+A simple PHP hit counter
 
+Version: 2.0.1
 Description:
    simPHP counts both regular and unique views on multiple
    webpages. The stats can be displayed on any PHP-enabled
-   webpage. You MUST have read/write permissions on files.
+   webpage. You MUST have read/write permissions on files
 
-Script by Ajay: ajay@scyberia.org
-http://scyberia.org
+Script by Ajay: me@ajay.ga
 
 ----------------------------*/
 
@@ -20,7 +20,7 @@ http://scyberia.org
 /*----------CONFIG----------*/
 
 // NOTE: If you change any config after using simphp,
-// remove the old files.
+// remove the old files
 
 // Relative URL of text file that holds hit info:
 $lf_name = "hits.txt";
@@ -35,17 +35,17 @@ $monthly = 1;
 //   oldfiles/6-12.txt
 $monthly_path = "oldfiles";
 
-// Count unique hits or all hits:
-//   0 = All hits
+// Count unique hits or total hits:
+//   0 = Total hits
 //   1 = Unique hits
-//   2 = Both
+//   2 = Both unique and total
 $type = 2;
 
 // Text to display
-// before all hits.
-$beforeAllText = "Hits: ";
+// before total hits
+$beforeTotalText = "Hits: ";
 
-// Before unique hits.
+// Before unique hits
 $beforeUniqueText = "Unique Visits: ";
 
 // Display hits on this page:
@@ -53,8 +53,8 @@ $beforeUniqueText = "Unique Visits: ";
 //   1 = Yes
 $display = 1;
 
-// Only change this if you are recording both values.
-// Separator for unique and all hits display - use HTML tags! (line break is default)
+// Only change this if you are recording both values
+// Separator for unique and total hits display - use HTML tags! (line break is default)
 $separator = "<br \>";
 
 // Default would output:
@@ -71,127 +71,210 @@ $separator = "<br \>";
 
 $log_file = dirname(__FILE__) . '/' . $lf_name;
 
-//Check for "?display=true" in URL.
+// Check for "?display=true" in URL
 if ($_GET['display'] == "true") {
-	//Show include() info.
+	// Show include() info
 	die("<pre>&#60;? include(\"" . dirname(__FILE__) . '/' . basename(__FILE__) . "\"); ?&#62;</pre>");
+
 } else {
-	//Visit or IP.
+	// Get visitor IP
 	$uIP = $_SERVER['REMOTE_ADDR'];
 
-	//Check for "hits.txt" file.
+	// Check for "hits.txt" file
 	if (file_exists($log_file)) {
-		//Check if today is first day of month
-		if (date('j') == 10) {
-			//Ensure that monthly dir exists
-			if (!file_exists($monthly_path)) {
-				mkdir($monthly_path);
-			}
-
-			//Check if prev month log file exists already
-			$prev_name = $monthly_path . '/' . date("n-Y", strtotime("-1 month"));
-			if (!file_exists($prev_name)) {
-				//If not, move/rename current file
-				copy($log_file, $prev_name);
-
-				//Create new $toWrite based on CONFIG
-				//Write file according to CONFIG above.
-				if ($type == 0) {
-					$toWrite = "1";
-					$info = $beforeAllText . "1";
-				} else if ($type == 1) {
-					$toWrite = "1;" . $uIP . ",";
-					$info = $beforeUniqueText . "1";
-				} else if ($type == 2) {
-					$toWrite = "1;1;" . $uIP . ",";
-					$info = $beforeAllText . "1" . $separator . $beforeUniqueText . "1";
-				}
-				goto write_logfile;
-			}
-		}
-
-		//Get contents of "hits.txt" file.
+		// Get contents of log file
 		$log = file_get_contents($log_file);
 
-		//Get type from CONFIG above.
-		if ($type == 0) {
+		if ($monthly) {
 
-			//Create info to write to log file and info to show.
-			$toWrite = intval($log) + 1;
-			$info = $beforeAllText . $toWrite;
+			// Check if today is first day of month
+			if (date('j') == 1) {
+				// If it is first day of month,
+				// move previous log file to subdir and create new file
 
-		} else if ($type == 1) {
+				// Ensure that monthly dir exists
+				if (!file_exists($monthly_path)) {
+					mkdir($monthly_path);
+				}
 
-			//Separate log file into hits and IPs.
-			$hits = reset(explode(";", $log));
-			$IPs = end(explode(";", $log));
-			$IPArray = explode(",", $IPs);
+				// Check if prev month log file exists already
+				$prev_name = $monthly_path . '/' . date("n-Y", strtotime("-1 month")) . '.txt';
+				if (!file_exists($prev_name)) {
+					// If not, copy current log file into subdir
+					copy($log_file, $prev_name);
 
-			//Check for visitor IP in list of IPs.
-			if (array_search($uIP, $IPArray, true) === false) {
-				//If doesnt' exist increase hits and include IP.
-				$hits = intval($hits) + 1;
-				$toWrite = $hits . ";" . $IPs . $uIP . ",";
+					// Write new data based on config
+					if ($type == 0) {
+						// Total hits
+						$toWrite = "1";
+						$info = $beforeTotalText . "1";
+					} else if ($type == 1) {
+						// Unique hits
+						$toWrite = "1;" . $uIP . ",";
+						$info = $beforeUniqueText . "1";
+					} else if ($type == 2) {
+						// Unique and total
+						$toWrite = "1;1;" . $uIP . ",";
+						$info = $beforeTotalText . "1" . $separator . $beforeUniqueText . "1";
+					}
+					write_logfile($toWrite, $info);
+				}
+
 			} else {
-				//Otherwise nothing.
-				$toWrite = $log;
+				// Still same month as before, so just increment counters
+
+				// What to do depends on type from config
+				if ($type == 0) {
+					// Total hits
+					// Create info to write to log file and info to show
+					$toWrite = intval($log) + 1;
+					$info = $beforeTotalText . $toWrite;
+
+				} else if ($type == 1) {
+
+					// Separate log file into hits and IPs
+					$hits = reset(explode(";", $log));
+					$IPs = end(explode(";", $log));
+					$IPArray = explode(",", $IPs);
+
+					// Check for visitor IP in list of IPs
+					if (array_search($uIP, $IPArray, true) === false) {
+
+						// IP doesnt' exist so increase hits and include IP
+						$hits = intval($hits) + 1;
+						$toWrite = $hits . ";" . $IPs . $uIP . ",";
+					} else {
+
+						// If IP exists don't change anything
+						$toWrite = $log;
+					}
+
+					// Info to show
+					$info = $beforeUniqueText . $hits;
+
+				} else if ($type == 2) {
+					// Both total hits and unique hits
+
+					// Separate log file into regular hits, unique hits, and IPs
+					$pieces = explode(";", $log);
+					$totalHits = $pieces[0];
+					$uniqueHits = $pieces[1];
+					$IPs = $pieces[2];
+					$IPArray = explode(",", $IPs);
+
+					// Always increase regular hits, regardless of IP
+					$totalHits = intval($totalHits) + 1;
+
+					// Search for visitor IP in list of IPs
+					if (array_search($uIP, $IPArray, true) === false) {
+
+						// IP doesn't exist so increase unique hits and append IP
+						$uniqueHits = intval($uniqueHits) + 1;
+						$toWrite = $totalHits . ";" . $uniqueHits . ";" . $IPs . $uIP . ",";
+					} else {
+
+						// If IP already exists just keep unique hits unchanged
+						$toWrite = $totalHits . ";" . $uniqueHits . ";" . $IPs;
+					}
+					// Info to show
+					$info = $beforeTotalText . $totalHits . $separator . $beforeUniqueText . $uniqueHits;
+				}
+				write_logfile($toWrite, $info);
 			}
-			//Info to show.
-			$info = $beforeUniqueText . $hits;
+		} else {
+			// What to do depends on type from config
+			if ($type == 0) {
+				// Total hits
+				// Create info to write to log file and info to show
+				$toWrite = intval($log) + 1;
+				$info = $beforeTotalText . $toWrite;
 
-		} else if ($type == 2) {
+			} else if ($type == 1) {
 
-			//Position of separators.
-			$c1Pos = strpos($log, ";");
-			$c2Pos = strrpos($log, ";");
+				// Separate log file into hits and IPs
+				$hits = reset(explode(";", $log));
+				$IPs = end(explode(";", $log));
+				$IPArray = explode(",", $IPs);
 
-			//Separate log file into regular hits, unique hits, and IPs.
-			$pieces = explode(";", $log);
-			$allHits = $pieces[0];
-			$uniqueHits = $pieces[1];
-			$IPs = $pieces[2];
-			$IPArray = explode(",", $IPs);
+				// Check for visitor IP in list of IPs
+				if (array_search($uIP, $IPArray, true) === false) {
 
-			//Increase regular hits.
-			$allHits = intval($allHits) + 1;
+					// IP doesnt' exist so increase hits and include IP
+					$hits = intval($hits) + 1;
+					$toWrite = $hits . ";" . $IPs . $uIP . ",";
+				} else {
 
-			//Search for visitor IP in list of IPs.
-			if (array_search($uIP, $IPArray, true) === false) {
-				//Increase ONLY unique hits and append IP.
-				$uniqueHits = intval($uniqueHits) + 1;
-				$toWrite = $allHits . ";" . $uniqueHits . ";" . $IPs . $uIP . ",";
-			} else {
-				//Else just include regular hits.
-				$toWrite = $allHits . ";" . $uniqueHits . ";" . $IPs;
+					// If IP exists don't change anything
+					$toWrite = $log;
+				}
+
+				// Info to show
+				$info = $beforeUniqueText . $hits;
+
+			} else if ($type == 2) {
+				// Both total hits and unique hits
+
+				// Separate log file into regular hits, unique hits, and IPs
+				$pieces = explode(";", $log);
+				$totalHits = $pieces[0];
+				$uniqueHits = $pieces[1];
+				$IPs = $pieces[2];
+				$IPArray = explode(",", $IPs);
+
+				// Always increase regular hits, regardless of IP
+				$totalHits = intval($totalHits) + 1;
+
+				// Search for visitor IP in list of IPs
+				if (array_search($uIP, $IPArray, true) === false) {
+
+					// IP doesn't exist so increase unique hits and append IP
+					$uniqueHits = intval($uniqueHits) + 1;
+					$toWrite = $totalHits . ";" . $uniqueHits . ";" . $IPs . $uIP . ",";
+				} else {
+
+					// If IP already exists just keep unique hits unchanged
+					$toWrite = $totalHits . ";" . $uniqueHits . ";" . $IPs;
+				}
+				// Info to show
+				$info = $beforeTotalText . $totalHits . $separator . $beforeUniqueText . $uniqueHits;
 			}
-			//Info to show.
-			$info = $beforeAllText . $allHits . $separator . $beforeUniqueText . $uniqueHits;
+			write_logfile($toWrite, $info);
 		}
 	} else {
-		//If "hits.txt" doesn't exist, create it.
-		$fp = fopen($log_file ,"w");
+		// If "hits.txt" doesn't exist, create it
+		$fp = fopen($log_file, "w");
 		fclose($fp);
 
-		//Write file according to CONFIG above.
+		// Write file according to config above
 		if ($type == 0) {
 			$toWrite = "1";
-			$info = $beforeAllText . "1";
+			$info = $beforeTotxalText . "1";
 		} else if ($type == 1) {
 			$toWrite = "1;" . $uIP . ",";
 			$info = $beforeUniqueText . "1";
 		} else if ($type == 2) {
 			$toWrite = "1;1;" . $uIP . ",";
-			$info = $beforeAllText . "1" . $separator . $beforeUniqueText . "1";
+			$info = $beforeTotalText . "1" . $separator . $beforeUniqueText . "1";
 		}
+		write_logfile($toWrite, $info);
 	}
+}
 
-	write_logfile:
-	//Put $toWrite in log file
-	file_put_contents($log_file, $toWrite);
+/**
+ * Writes given data to the logfile and echoes data if the option
+ * 	 says so in config
+ * Requires: A string of data to write to the file and a string
+ *   of data to print
+ */
+function write_logfile($data, $output) {
+	global $log_file;
 
-	//Display info if is set in CONFIG.
+	// Put $toWrite in log file
+	file_put_contents($log_file, $data);
+
+	// Display info if is set in config
 	if ($display == 1) {
-		echo $info;
+		echo $output;
 	}
 }
 ?>
